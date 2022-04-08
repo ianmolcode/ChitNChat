18f1119c61c9b24f2808d3901aa59405845ec0c1
Anmol Narang <nanmol0406@gmail.com>
2022-04-08 17:02:57 +0530

    Update 1

--- app/src/main/java/com/example/chitnchat/SignUp.java
+++ app/src/main/java/com/example/chitnchat/SignUp.java
@@ -1,14 +1,50 @@
 package com.example.chitnchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.chitnchat.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
+    ActivitySignUpBinding binding ;
+    private FirebaseAuth auth ;
+    FirebaseDatabase database ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        auth = FirebaseAuth.getInstance() ;
+        database = FirebaseDatabase.getInstance();
+        //setting sign up button
+        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.createUserWithEmailAndPassword(binding.idemailSi.getText().toString() , binding.idpasswordSi.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                     if(task.isSuccessful()){
+                         Toast.makeText(SignUp.this, "User Created Successfully", Toast.LENGTH_SHORT).show();
+                     }
+                     else
+                         Toast.makeText(SignUp.this, "", Toast.LENGTH_SHORT).show();
+                        {
+                            Toast.makeText(SignUp.this, task.getException().getMessage() , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
---
+++ app/src/test/java/Model/Users.java
@@ -0,0 +1,69 @@
+package Model;
+
+public class Users {
+    String profilePic , username , password , mail , userID , lastMessage ;
+
+    public Users(String profilePic, String username, String password, String mail, String userID, String lastMessage) {
+        this.profilePic = profilePic;
+        this.username = username;
+        this.password = password;
+        this.mail = mail;
+        this.userID = userID;
+        this.lastMessage = lastMessage;
+    }
+    public Users(){}
+    //sign up constructor
+    public Users(String username , String password , String mail )
+    {
+        this.username =  username ;
+        this.password = password ;
+        this.mail = mail ;
+    }
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+}
